75f4efdd2e76e01920f1c96f7265468a326afe73
haeter525 <cindejze412@gmail.com>
2022-07-25 23:57:30 +0800

    Add clear text check using ciphey

---
+++ tests/script/test_ciphey.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# This file is part of Quark-Engine - https://github.com/quark-engine/quark-engine
+# See the file 'LICENSE' for copying permission.
+
+from quark.script.ciphey import checkClearText
+
+
+def testCheckClearTextWithClearText():
+    assert checkClearText("Clear Text") is True
+
+
+def testCheckClearTextWithCipherText():
+    assert checkClearText("NB2HI4DTHIXS6Z3PN5TWYZJOMNXW2===") is False
